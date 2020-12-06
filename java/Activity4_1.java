8c127757fa56eb4f4e7a996c6ce7807e275f2e5d
teenamathew1 <75238447+teenamathew1@users.noreply.github.com>
2020-12-07 00:04:37 +0530

    Add files via upload

---
+++ java/Activity4_1.java
@@ -0,0 +1,36 @@
+package JavaActivity4;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+//Random Scanner Activity
+public class Activity4_1 {
+
+	public static void main(String[] args) {
+		
+		//Create a Scanner object
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Random indexGen = new Random();
+		
+		System.out.print("Enter integers please ");
+        System.out.println("(EOF or non-integer to terminate): ");
+        
+		while(scan.hasNextInt())
+		{
+			list.add(scan.nextInt());
+		}
+		
+		Integer nums[] = list.toArray(new Integer[0]);		
+		int index = indexGen.nextInt(nums.length);
+		
+		System.out.println("Index value generated: " + index);
+        System.out.println("Value in arary at generated index: " + nums[index]);
+        
+        scan.close();
+
+	}
+
+}
