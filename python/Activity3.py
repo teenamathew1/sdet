563a1a36e11ababafcb80a76f9b9858f45600902
teenamathew1 <75238447+teenamathew1@users.noreply.github.com>
2020-12-08 23:21:12 +0530

    Add files via upload

---
+++ python/Activity3.py
@@ -0,0 +1,36 @@
+"""
+
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock
+"""
+	
+# Get the users names
+user1 = input("What is player1's name? ")
+user2 = input("What is player2's name? ")
+
+# Get the user choices
+user1_ans = input(user1 + ", do you want to choose rock, paper or scissors? ").lower()
+user2_ans = input(user2 + ", do you want to choose rock, paper or scissors? ").lower()
+
+if user1_ans == user2_ans:
+    print("Its a tie!")
+elif user1_ans == 'rock':
+    if user2_ans == 'scissors':
+        print("rock wins")
+    else:
+        print("paper wins!")
+elif user1_ans == 'scissors':
+    if user2_ans == 'paper':	
+        print("Scissors win!")
+    else:
+        print("Rock wins!")
+elif user1_ans == 'paper':
+    if user2_ans == 'rock':
+        print("Paper wins!")
+    else:
+        print("Scissors win!")
+else:
+    print("Invalid input! You have not entered rock, paper or scissors, try again.")
+        
+
