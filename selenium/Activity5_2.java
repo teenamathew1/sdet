1f97a97cd5f9e3096b65ed736b668b25068a8b4c
teenamathew1 <75238447+teenamathew1@users.noreply.github.com>
2020-12-17 00:33:13 +0530

    Add files via upload

---
+++ selenium/Activity5_2.java
@@ -0,0 +1,35 @@
+package SeleniumActivities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity5_2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        //Open the browser
+        driver.get("https://www.training-support.net/selenium/dynamic-controls");
+
+        //Find the checkbox
+        WebElement checkboxInput = driver.findElement(By.xpath("//input[@type='checkbox']"));
+        System.out.println("The checkbox is selected: " + checkboxInput.isSelected());
+
+        Thread.sleep(900);
+        //Select the checkbox
+        checkboxInput.click();
+
+        Thread.sleep(500);
+        //Print status
+        System.out.println("The checkbox is selected: " + checkboxInput.isSelected());
+
+        //Close the browser
+        driver.close();
+
+    }
+
+}
